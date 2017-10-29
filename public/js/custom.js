cba7fe46074eaa861fbd0ac6a1eff7ed224274c6
Siddhant2016 <harshsrivastav123@gmail.com>
2017-10-29 10:28:54 +0530

    Ui updation

---
+++ public/js/custom.js
@@ -0,0 +1,30 @@
+﻿(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
